f7bfd12312abf346eb1a89698d5952f326a187ce
Rishaviq <juliqnbg@gmail.com>
2025-07-15 19:59:20 +0300

    Add JWT authentication and DI for services and repositories
    Configured JWT authentication in Program.cs and registered repositories, services, and helpers for dependency injection. Updated PoolGame.WebAPI.csproj to include necessary package and project references. Added launchSettings.json for the Test project.

--- PoolGame.WebAPI/Program.cs
+++ PoolGame.WebAPI/Program.cs
@@ -1,4 +1,23 @@
 
+using PoolGame.Repositories.Implementations.Game;
+using PoolGame.Repositories.Implementations.PlayerStat;
+using PoolGame.Repositories.Implementations.User;
+using PoolGame.Repositories.Interfaces.Game;
+using PoolGame.Repositories.Interfaces.PlayerStat;
+using PoolGame.Repositories.Interfaces.User;
+using PoolGame.Services.Interfaces.Game;
+using PoolGame.Services.Interfaces.PlayerStat;
+using PoolGame.Services.Interfaces.User;
+using PoolGame.Services.Implementations.Game;
+using PoolGame.Services.Implementations.PlayerStat;
+using PoolGame.Services.Implementations.User;
+using PoolGame.Services.Helpers.Interfaces;
+using PoolGame.Services.Helpers.Implementations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using PoolGameAPI.Controllers;
+
 namespace PoolGame.WebAPI
 {
     public class Program
@@ -6,6 +25,45 @@ namespace PoolGame.WebAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+
+            //adding repositories 
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IPlayerStatRepository, PlayerStatRepository>();
+            builder.Services.AddScoped<IGameRepository, GameRepository>();
+
+
+            //adding services
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IPlayerStatService, PlayerStatService>();
+            builder.Services.AddScoped<IGameService, GameService>();
+
+
+            //adding helpers
+            builder.Services.AddSingleton<IPasswordHasher, PasswordHasher>();
+            builder.Services.AddSingleton<ITokenProvider, TokenProvider>();
+
+
+            builder.Services.AddAuthorization();
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(o =>
+                {
+                    o.RequireHttpsMetadata = false;
+                    o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]!)),
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        ClockSkew = TimeSpan.Zero
+
+                    };
+
+                });
+
+
+
+
 
             // Add services to the container.
 
@@ -24,7 +82,7 @@ namespace PoolGame.WebAPI
             }
 
             app.UseHttpsRedirection();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
